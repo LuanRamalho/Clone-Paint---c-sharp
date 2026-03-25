ba1730b3a8ea91fd2d5f170c2c812112401f9b30
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2026-03-25 14:16:22 -0300

    Enums.cs

--- Paint/Enums.cs
+++ Paint/Enums.cs
@@ -1,5 +1,5 @@
 namespace Win11PaintClone
 {
-    public enum DrawingTool { Pencil, Eraser, Line, Rectangle, Ellipse, Triangle, Star, Arrow, Fill }
+    public enum DrawingTool { Pencil, Eraser, Line, Rectangle, Square, Ellipse, Triangle, Pentagon, Hexagon, Star, Arrow, Fill }
     public enum BrushType { Standard, Calligraphy, Spray }
 }
